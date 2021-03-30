476563e8a93b8aa726b053c865130141a9b9aab7
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-30 16:45:37 -0500

    create booklist container component

--- src/Containers/BooksList.js
+++ src/Containers/BooksList.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const BooksList = () => {
-  return ( 
-    <h3>Bookslist</h3>
-   );
-}
- 
-export default BooksList;
+import Book from '../Components/Book';
+import { createbook, removebook } from '../Actions/index';
+
+const BooksList = ({ books }) => (
+  <table>
+    <tr>
+      <th>
+        BookID
+      </th>
+      <th>
+        Title
+      </th>
+      <th>
+        Category
+      </th>
+    </tr>
+    {books.map((book) => <Book key={book} book={book} />)}
+  </table>
+);
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+const mapStateProps = (state) => ({ books: state.books });
+const mapDispatchToProps = (dispatch) => ({
+  create: (book) => dispatch(createbook(book)),
+  delete: (book) => dispatch(removebook(book)),
+});
+
+export default connect(mapStateProps, mapDispatchToProps)(BooksList);
