e73f18f5171ede5f16a32046f011748105d99f25
mke2111 <roymke134@gmail.com>
2021-03-31 01:30:58 +0300

    Store the initial state of books in redux store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 import App from './Components/App';
-import store from './app/store';
+
+const initialState = [
+  { id: Math.floor(Math.random() * 100) + 1, title: 'Book1', category: 'Horror' },
+  { id: Math.floor(Math.random() * 100) + 1, title: 'Book2', category: 'Action' },
+  { id: Math.floor(Math.random() * 100) + 1, title: 'Book3', category: 'Biography' },
+  { id: Math.floor(Math.random() * 100) + 1, title: 'Book4', category: 'History' },
+  { id: Math.floor(Math.random() * 100) + 1, title: 'Book5', category: 'Kids' },
+  { id: Math.floor(Math.random() * 100) + 1, title: 'Book6', category: 'Learning' },
+  { id: Math.floor(Math.random() * 100) + 1, title: 'Book7', category: 'Sci-Fi' },
+];
+
+const store = createStore(reducer, { books: initialState });
 
 ReactDOM.render(
   <React.StrictMode>
