cf15c37db45fbb439eb8829c44f0c5b77b8520a7
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-30 16:59:13 -0500

    add the bookform to add the books

--- src/Containers/BooksForm.js
+++ src/Containers/BooksForm.js
@@ -1,9 +1,30 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 const BooksForm = () => {
-  return ( 
-    <h3>Booksform</h3>
-   );
-}
- 
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+  return (
+    <>
+      <form>
+        <label>
+          Book title
+          <input type="text" />
+        </label>
+        <label>
+          Select the Category
+          <select>
+            {categories.map((cat) => (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Add the book</button>
+      </form>
+    </>
+  );
+};
+
 export default BooksForm;
