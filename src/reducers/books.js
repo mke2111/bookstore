6051345dbd4e7d2406a954728b12afe450b651fa
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-31 10:53:43 -0500

    add reduce filter for catgories

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,11 +1,18 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../Actions/index';
 
+export const randInt = (minim, maxim) => {
+  const min = Math.ceil(minim);
+  const max = Math.floor(maxim);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 export default (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK:
-      return {
-        ...state, book: action.book,
-      };
+      return [
+        // eslint-disable-next-line max-len
+        ...state, { id: randInt(1, 1000), title: action.book.title, category: action.book.category },
+      ];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.book.id);
     default:
---
+++ src/reducers/filter.js
@@ -0,0 +1,10 @@
+import { CHANGE_FILTER } from '../Actions';
+
+export default (state = 'All', action) => {
+  switch (action.type) {
+    case CHANGE_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+};
