8666b3ba9ee6357965ea57dcb7875a509299884b
mke2111 <roymke134@gmail.com>
2021-04-01 01:37:17 +0300

    Add progress bar

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../index.css';
+import '../css-circular-prog-bar.css';
 
 const Book = ({ book, delete: handleRemoveBook }) => (
-  <div>
+  <div className="d-flex flex-row justify-content-around book-margin">
     {/* <tr>
       <td>
         {book.id}
@@ -34,8 +36,20 @@ const Book = ({ book, delete: handleRemoveBook }) => (
         <h3>Edit</h3>
       </div>
     </div>
-    <div>
+    <div className="d-flex align-items-center">
+      <div className="progress-circle p10">
+        <span>10%</span>
+        <div className="left-half-clipper">
+          <div className="first50-bar" />
+          <div className="value-bar" />
+        </div>
+      </div>
       <h3>64%</h3>
+    </div>
+    <div>
+      <h3>Current Chapter</h3>
+      <h3>Chapter 16</h3>
+      <button type="button">Update progress</button>
     </div>
   </div>
 );
